d6a8ea335eef5b7b873279302286c80fc15adf14
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-22 15:58:06 -0500

    Initial Commit

---
+++ Localization2023Demo/Localization2023DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Localization2023DemoApp.swift
+//  Localization2023Demo
+//
+//  Created by Mark Volkmann on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Localization2023DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
